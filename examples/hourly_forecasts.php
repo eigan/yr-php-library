06c86605ca3ac1044a062d0dbcc4bfbbd2854045
Einar <einargangso@gmail.com>
2013-05-06 01:00:16 +0200

    converting indentation to spaces

--- examples/hourly_forecasts.php
+++ examples/hourly_forecasts.php
@@ -5,6 +5,6 @@ include "../Yr.php";
 $yr = eigan\yr\Yr::create("Norway/Vestfold/Sandefjord/Sandefjord", "/tmp/");
 
 foreach($yr->getHourlyForecasts() as $forecast) {
-	print $forecast->getFrom()->format("H:i") . ": " . $forecast->getTemperature() . "\n";
+    print $forecast->getFrom()->format("H:i") . ": " . $forecast->getTemperature() . "\n";
 }
 
